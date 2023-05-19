1a7e722ef71508d09f9a3c4efb17eca0d8e95e1a
rashad <akyrashad@gmail.com>
2023-05-19 07:41:23 +0530

    live wire samples

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class HelloWorld extends Component
 {
+    public $name = 'Rashad';
+    public $loud = false;
+    public $greeting  = ['Hello'];
+
+
+    public function resetName($name = 'Default')
+    {
+        $this->name = $name;
+    }
     public function render()
     {
         return view('livewire.hello-world');
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,20 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
+
+    <input type="text" wire:model="name">
+    <input type="checkbox" wire:model="loud">
+    <select wire:model="greeting" multiple>
+        <option>Hello</option>
+        <option>Goodbye</option>
+        <option>Adios</option>
+    </select>
+
+    {{ implode(',', $greeting) }} {{ $name }} @if ($loud)
+        !
+    @endif
+
+    <form action="" wire:submit.prevent="$set('name','Bingo')">
+        <button>Reset Name</button>
+    </form>
+
+
 </div>
